b60d6c73d07f19b11033c1c293731792ed4ae891
umairanoufal <pooviumaira@gmail.com>
2021-10-07 13:34:41 +0530

    initial commit

---
+++ sample.js
@@ -0,0 +1,65 @@
+// // // simple calculator
+// var op=window.prompt("Select OPERATION : + - / * ");
+// var a = parseInt(window. prompt("OPRAND 1: "));
+// var b = parseInt(window. prompt("OPRAND 2: "));
+
+// document.write(a+ op+ b + " = ");
+// switch(op)
+//     {
+//         case '+':
+//             document.write(a+b);
+//             break;
+//         case '-':
+//             document.write(a-b);
+//             break;
+//         case '/':
+//             document.write(a/b);
+//             break;
+//         case '*':
+//             document.write(a*b);
+//             break;
+//     }
+
+// // add two numbers entered by the user:
+// var num1=parseInt(window.prompt("Enter no.1 : "));
+// var num2=parseInt(window.prompt("Enter no.2 : "));
+// document.write(num1+ "+" + num2+ " = "+ (num1+num2));
+
+// // Check whether the number is odd or even
+// var num=parseInt(window.prompt("Enter the number: "));
+// if(num%2==0)
+//     document.write(num+ " : EVEN NO!")
+// else    
+//     document.write(num+ " : ODD NO.!");
+
+// // LARGEST AMONG THREE NUMBERS:
+// var a=parseInt(window.prompt("no 1: "));
+// var b=parseInt(window.prompt("no 2: "));
+// var c=parseInt(window.prompt("no 3: "));
+// if(a>b)
+//     {
+//         if (a>c)
+//             document.write(a+" - LARGEST");
+//         else
+//         document.write(a+" - LARGEST");
+//     }
+// else if (b>c)
+//     document.write(b+" - LARGEST");
+
+//     else 
+//     document.write(c+" - LARGEST");
+
+
+// // POSITIVE/ NEGATIVE/ ZERO
+var no=parseInt(window.prompt("enter a no: "));
+if (no>0)
+    document.write(no + " -- > POSTIVE");
+else if(no<0)
+    document.write(no + " -- > NEGAIVE");
+else
+    document.write(no + " -- > ZERO");
+
+
+    
+
+
